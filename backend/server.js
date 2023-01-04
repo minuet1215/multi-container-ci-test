67442885cddbc2702a6c6c1db1724aa21d57ee3a
minu <brotheratoz3@naver.com>
2023-01-04 23:50:04 +0900

    feat: db table 생성 코드 추가

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+db.pool.query(
+  `CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`,
+  (err, resuts, fields) => {
+    console.log("results", results);
+  }
+);
+
 app.get("/api/values", (req, res) => {
-  db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
+  db.pool.query("SELECT * FROM lists;", (err, results, fields) => {
     if (err) {
       return res.status(500).send(err);
     }
@@ -19,7 +30,7 @@ app.get("/api/values", (req, res) => {
 app.post("/api/value", (req, res) => {
   db.pool.query(
     `INSERT INTO lists (value) VALUES("${req.body.value}")`,
-    (err, results, fileds) => {
+    (err, results, fields) => {
       if (err) {
         return res.status(500).send(err);
       }

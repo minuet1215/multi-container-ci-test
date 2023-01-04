6e72eb2c437dc26ebb975b930b9c584f1f7a6954
minu <brotheratoz3@naver.com>
2023-01-04 23:44:48 +0900

    feat: db table의 모든 data를 가져오는 api 구현

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get("/api/values", (req, res) => {
+  db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.json(results);
+  });
+});
+
 app.listen(5000, () => {
   console.log("server listened on 5000");
   console.log(db);

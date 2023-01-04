084a5a20b210b4bd692a250125a68aa807e40a0e
minu <brotheratoz3@naver.com>
2023-01-04 23:41:41 +0900

    build: mysql 연동

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const db = require("./db");
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.listen(5000, () => {
   console.log("server listened on 5000");
+  console.log(db);
 });

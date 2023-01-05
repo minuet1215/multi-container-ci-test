996716a1d306decf3c43c1da0771ad418b654202
minu <brotheratoz3@naver.com>
2023-01-06 00:05:11 +0900

    chore

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASES,
   port: process.env.MYSQL_PORT,
 });
 
